b60c2c954e57dd9e3a5c7286c6bc2617f3015b98
Nathan Campos <nathan@innoveworkshop.com>
2024-04-02 14:45:53 +0100

    beginning of save input
    self explanatory, created a script that WOULD send last button to a WOULD BE script.
    And added tags to the piano keys.

---
+++ Assets/scripts/StoreKeyPress.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreKeyPress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(gameObject.tag);
+    }
+
+
+    public void SaveInput()
+	{
+
+	}
+}
